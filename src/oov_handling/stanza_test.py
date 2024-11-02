a904ab44701d16153881065c1bc0138a3ebf5d7c
Angelos Toutsios <angelos.toutsios@gmail.com>
2024-11-01 20:29:09 -0700

    add to git ignore db files

--- src/oov_handling/stanza_test.py
+++ src/oov_handling/stanza_test.py
@@ -1,13 +1,15 @@
 import stanza
 from stanza.pipeline.core import DownloadMethod
 
-stanza.download('en', processors='tokenize,pos', logging_level='WARN')
+stanza.download('en', processors='tokenize,pos,lemma', logging_level='WARN')
 
-nlp = stanza.Pipeline('en', processors='tokenize,pos', download_method=DownloadMethod.REUSE_RESOURCES, logging_level='WARN')
+# nlp = stanza.Pipeline('en', processors='tokenize,pos', download_method=DownloadMethod.REUSE_RESOURCES, logging_level='WARN')
+nlp = stanza.Pipeline('en', processors='tokenize,pos,lemma')
 
 doc = nlp('Barack Obama was born in Hawaii.')
 
 for sentence in doc.sentences:
     for word in sentence.words:
         print(word.text, word.lemma, word.pos)
+
 
