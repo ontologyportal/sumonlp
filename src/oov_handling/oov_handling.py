06308c0bc8b9bbea773fca4b759876564c7e4005
Angelos Toutsios <angelos.toutsios@gmail.com>
2024-11-12 22:07:51 -0800

    add NER in addition to OOV

--- src/oov_handling/oov_handling.py
+++ src/oov_handling/oov_handling.py
@@ -4,11 +4,12 @@ import os
 import time
 
 # Initialize Stanza pipeline for English
-stanza.download('en', processors='tokenize,pos,lemma', verbose=False)  # Download model if not already done
-nlp = stanza.Pipeline('en', processors='tokenize,pos,lemma', verbose=False)
+stanza.download('en', processors='tokenize,pos,lemma,ner', verbose=False)  # Download model if not already done
+nlp = stanza.Pipeline('en', processors='tokenize,pos,lemma,ner', verbose=False)
 
 # Database configuration
-DB_PATH = '/data/angelos.toutsios.gr/vocabulary.db'
+# DB_PATH = '/data/angelos.toutsios.gr/vocabulary_test.db'
+DB_PATH = '/home/angelos.toutsios.gr/workspace/sumonlp/src/oov_handling/vocabulary_test.db'
 
 # Connect to the SQLite database
 conn = sqlite3.connect(DB_PATH)
@@ -19,6 +20,8 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS UnknownWords (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     word TEXT UNIQUE,
+    formatted_word TEXT DEFAULT '',
+    type TEXT DEFAULT '',
     used INTEGER DEFAULT 0 CHECK (used IN (0, 1))
 )
 """)
@@ -28,8 +31,8 @@ def get_word_type(word):
     """Determine if the word is a noun or verb using Stanza."""
     if word.upos == 'NOUN':
         return 'noun'
-    elif word.upos == 'PROPN':
-      return 'noun-phrase'
+    # elif word.upos == 'PROPN':
+    #   return 'noun-phrase'
     elif word.upos == 'VERB':
         return 'verb'
     else:
@@ -41,18 +44,18 @@ def check_word_in_dictionary(root, word_type):
     # cursor.execute("SELECT id FROM Dictionary WHERE root = ?", (root.lower()))
     return cursor.fetchone()
 
-def add_unknown_word(word, root, word_type):
+def add_unknown_word(word, word_type):
     """Check if an unknown word already exists in the UnknownWords table with 'used' set to 0, or insert it if not."""
     try:
         # trim word
         word = word.strip()
         word = word.lower()
-        cursor.execute("SELECT * FROM UnknownWords WHERE word = ? AND used = 0", (word,))
+        cursor.execute("SELECT * FROM UnknownWords WHERE word = ? AND used = 0 AND type = ?", (word, word_type))
         result = cursor.fetchone()
         if result:
             return result[0] # Return the ID if the word exists
         else:
-            cursor.execute("INSERT INTO UnknownWords (word) VALUES (?)", (word,))
+            cursor.execute("INSERT INTO UnknownWords (word, type) VALUES (?,?)", (word, word_type))
             conn.commit()
             return cursor.lastrowid # Return the ID of the newly inserted word
     except sqlite3.IntegrityError as e:
@@ -62,14 +65,15 @@ def add_unknown_word(word, root, word_type):
 
 def process_sentence(sentence):
     """Process a single sentence, replacing unknown nouns and verbs with tags."""
+
     doc = nlp(sentence)
     processed_tokens = []
 
+    # Check NOUNS/VERBS in Vocabulary
     for sent in doc.sentences:
         for word in sent.words:
             word_type = get_word_type(word)
             if word_type:
-
                 # Check if the root form exists in the Dictionary
                 dictionary_entry = check_word_in_dictionary(word.lemma, word_type)
                 if dictionary_entry:
@@ -77,7 +81,7 @@ def process_sentence(sentence):
                     processed_tokens.append(word.text)
                 else:
                     # Unknown word, replace with <UNK_type_id>
-                    unk_id = add_unknown_word(word.text , word.lemma, word_type)
+                    unk_id = add_unknown_word(word.text, word_type)
                     if unk_id != None:
                         processed_tokens.append(f"<UNK_{word_type}_{unk_id}>")
                     else:
@@ -88,7 +92,24 @@ def process_sentence(sentence):
                 processed_tokens.append(word.text)
 
     # Join processed tokens to form the sentence with appropriate spacing
-    return ' '.join(processed_tokens)
+    new_sentence = ' '.join(processed_tokens)
+
+    doc = nlp(new_sentence)
+
+    # NER process  {ent.text} {ent.type}
+    for ent in doc.ents:
+
+      # Save each ent and type in DB
+      unk_id = add_unknown_word(ent.text, ent.type)
+
+      # Replace in the document each ent with the appropiate <tag>
+      tag = f'<UNK_{ent.type}_{unk_id}>'
+      new_sentence = new_sentence.replace(ent.text, tag)
+
+    return new_sentence
+
+
+
 
 def process_file(input_file, output_file):
     """Process the input file and write the processed text to the output file."""
